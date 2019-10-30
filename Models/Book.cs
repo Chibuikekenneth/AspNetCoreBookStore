5964e601f9f71cf296b705da2643340073ee5181
CHIBUIKE KENNETH <Chibuikekenneth003@gmail.com>
2019-10-30 19:40:40 +0100

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBookStore.Models
+{
+    public class Book
+    {
+        public int BookId {  get; set; }
+        public String Name { get; set; }
+        public string ShortDescription { get; set; }
+    }
+}
